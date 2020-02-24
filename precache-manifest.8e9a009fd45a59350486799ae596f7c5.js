6923b1dfd15a3acaee7f9652419a032e6d0da64b
minsung kang <minsung.kang929@gmail.com>
2020-02-24 16:24:45 -0500

    Updates

---
+++ precache-manifest.8e9a009fd45a59350486799ae596f7c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2794a967985eb5f2a3a18194c635695e",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "5f3ca04494733bd46269",
+    "url": "/movie/static/css/main.2ce72dcc.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/movie/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "5f3ca04494733bd46269",
+    "url": "/movie/static/js/main.c4fd7482.chunk.js"
+  },
+  {
+    "revision": "7954298017ed2d5ed8bc",
+    "url": "/movie/static/js/runtime~main.038a1c73.js"
+  }
+]);
